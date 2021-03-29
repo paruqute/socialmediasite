53a7cac41adc6c83dc3d968c10634072145e3e5e
paruqute <parvathychandran.t@gmail.com>
2021-03-29 15:44:00 +0530

    website is ready

--- output.php
+++ output.php
@@ -11,7 +11,7 @@
 <body >
     <nav class="navbar navbar-expand-lg navbar-light bg-warning">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#"></a>
+          <a class="navbar-brand" href="#">Project</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
@@ -76,6 +76,6 @@
             </div>
         </div>
     </div>
-    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script> 
 </body>
 </html>
